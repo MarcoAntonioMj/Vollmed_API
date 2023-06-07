55e1b5fdba39d9b56d31ac98055f58a33d3928fa
Marco Antonio <marcotonho38@gmail.com>
2023-06-07 20:00:17 -0300

    Refactor : corrigindo mapping medico

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import java.util.List;
-import java.util.ArrayList;
 
 
 
@@ -30,7 +31,10 @@ public class MedicoController {
         // Imprime os dados recebidos no console
       repository.save(new Medico(dados));
     }
-   public List<Medico> listar(){
-    
+
+    @GetMapping
+   public List<DadosListagemMedico> listar(){
+    return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+
    }
 }
