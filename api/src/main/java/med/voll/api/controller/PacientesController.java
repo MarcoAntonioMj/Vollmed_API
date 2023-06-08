d5b49cc94f09a8278a14af39d0b8fb71cef9812f
Marco Antonio <marcotonho38@gmail.com>
2023-06-08 19:50:50 -0300

    FIX : CORRIGINDO Pacientes Controller

--- api/src/main/java/med/voll/api/controller/PacientesController.java
+++ api/src/main/java/med/voll/api/controller/PacientesController.java
@@ -3,13 +3,19 @@ package med.voll.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
 
+
+import jakarta.validation.Valid;
+import med.voll.api.pacientes.DadosCadastroPaciente;
 import med.voll.api.pacientes.DadosListagenPaciente;
+import med.voll.api.pacientes.Paciente;
 import med.voll.api.pacientes.PacientesRepository;
 
 @RestController
@@ -21,8 +27,11 @@ public class PacientesController {
 
     @PostMapping
     @Transactional
-    public Page <DadosListagenPaciente> listar(@PageableDefault(size = 10, sort = {"nome"} ) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagenPaciente::new);
-
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
+    @GetMapping
+public Page<DadosListagenPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+    return repository.findAll(paginacao).map(DadosListagenPaciente::new);
+}
 }
