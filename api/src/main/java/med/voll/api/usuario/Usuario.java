bdd5fad9e966328772f07e8f6a2f65e32343ae56
Marco Antonio <marcotonho38@gmail.com>
2023-06-27 21:00:19 -0300

    FEAT : add novo clase Usuario

---
+++ api/src/main/java/med/voll/api/usuario/Usuario.java
@@ -0,0 +1,23 @@
+package med.voll.api.usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Usuario {
+
+    private Long id;
+    private String login;
+    private String senha;
+
+}
